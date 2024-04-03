f59a9836cb153792356bead3f8619d8916396dff
Anik <anikdasjoy474@gmail.com>
2024-04-03 16:46:14 +0600

    get cookie and clear cookie for login and logout

---
+++ src/pages/CheckOut/CheckOut.jsx
@@ -0,0 +1,45 @@
+// import { useLoaderData } from "react-router-dom";
+
+// const CheckOut = () => {
+//     const service = useLoaderData();
+//     const { title, _id } = service;
+//     return (
+//         <div>
+//             <h2>Book services: {title}</h2>
+//             <form >
+//                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+//                     <div className="form-control">
+//                         <label className="label">
+//                             <span className="label-text">Email</span>
+//                         </label>
+//                         <input type="text" placeholder="email" className="input input-bordered" required />
+//                     </div>
+//                     <div className="form-control">
+//                         <label className="label">
+//                             <span className="label-text">Password</span>
+//                         </label>
+//                         <input type="text" placeholder="password" className="input input-bordered" required />
+//                     </div>
+//                     <div className="form-control">
+//                         <label className="label">
+//                             <span className="label-text">Email</span>
+//                         </label>
+//                         <input type="text" placeholder="email" className="input input-bordered" required />
+//                     </div>
+//                     <div className="form-control">
+//                         <label className="label">
+//                             <span className="label-text">Password</span>
+//                         </label>
+//                         <input type="text" placeholder="password" className="input input-bordered" required />
+//                     </div>
+//                 </div>
+//                 <div className="form-control mt-6">
+//                     <button className="btn btn-primary btn-block" type="submit" value="Order Confirm"></button>
+//                 </div>
+//             </form>
+//         </div>
+
+//     );
+// };
+
+// export default CheckOut;
